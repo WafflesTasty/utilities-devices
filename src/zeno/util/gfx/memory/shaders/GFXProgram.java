07d1c7a2c2a18e6c8ac71a5d21c982a238cabe06
X-Zeno-X <zenothedefiant@gmail.com>
2020-05-12 19:34:31 +0200

    Initial commit.

--- src/zeno/util/gfx/memory/shaders/GFXProgram.java
+++ src/zeno/util/gfx/memory/shaders/GFXProgram.java
@@ -2,44 +2,49 @@ package zeno.util.gfx.memory.shaders;
 
 import zeno.util.gfx.GFXMemory;
 import zeno.util.tools.patterns.manipulators.Activator;
-import zeno.util.tools.patterns.manipulators.Disposable;
 import zeno.util.tools.patterns.manipulators.Loadable;
-import zeno.util.tools.patterns.properties.Discernible;
 
 /**
- * The {@code GFXProgram} interface defines a shader pipeline generated in {@link GFXMemory}.
+ * The {@code GFXProgram} interface defines a {@code GFXShader} pipeline program.
  * 
- * @since Sep 8, 2016
  * @author Zeno
+ * @since Sep 8, 2016
+ * @version 1.0
  * 
- * @see Discernible
- * @see Disposable
+ * 
  * @see Activator
+ * @see GFXMemory
  * @see Loadable
  */
-public interface GFXProgram extends Activator, Discernible, Disposable, Loadable
+public interface GFXProgram extends GFXMemory.Data, Activator, Loadable
 {
-	/**
-	 * Removes a shader from the {@code GFXProgram}.
-	 * 
-	 * @param shader  a shader to remove
-	 * @see GFXShader
-	 */
-	public abstract void removeShader(GFXShader shader);
-
 	/**
 	 * Adds a shader to the {@code GFXProgram}.
 	 * 
 	 * @param shader  a shader to add
+	 * 
+	 * 
 	 * @see GFXShader
 	 */
-	public abstract void addShader(GFXShader shader);
+	public abstract void add(GFXShader shader);
+	
+	/**
+	 * Removes a shader from the {@code GFXProgram}.
+	 * 
+	 * @param shader  a shader to remove
+	 * 
+	 * 
+	 * @see GFXShader
+	 */
+	public abstract void remove(GFXShader shader);
 
 	/**
-	 * Returns a generated error message.
+	 * Returns an error from the {@code GFXProgram}.
 	 * 
 	 * @return  an error message
+	 * 
+	 * 
 	 * @see String
 	 */
-	public abstract String getErrorMessage();
+	public abstract String ErrorMessage();
 }
