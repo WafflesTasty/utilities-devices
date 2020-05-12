69c556e7b3ea2ad45b2f9738f68ec8796292b0e1
X-Zeno-X <zenothedefiant@gmail.com>
2020-05-12 23:58:06 +0200

    Initial commit.

---
+++ src/zeno/util/gfx/system/memory/GFXGroup.java
@@ -0,0 +1,20 @@
+package zeno.util.gfx.system.memory;
+
+import zeno.util.gfx.system.GFXMemory;
+import zeno.util.tools.patterns.manipulators.Activator;
+
+/**
+ * The {@code GFXGroup} interface defines a format group generated in {@link GFXMemory}.
+ * 
+ * @author Zeno
+ * @since Sep 8, 2016
+ * @version 1.0
+ * 
+ * 
+ * @see GFXMemory
+ * @see Activator
+ */
+public interface GFXGroup extends GFXMemory.Data, Activator
+{		
+	// NOT APPLICABLE
+}
