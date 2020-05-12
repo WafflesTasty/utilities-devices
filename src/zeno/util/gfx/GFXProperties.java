89696af48a7803f166759346f171e09dc06aed57
X-Zeno-X <zenothedefiant@gmail.com>
2020-05-12 04:08:24 +0200

    Initial commit.

--- src/zeno/util/gfx/GFXProperties.java
+++ src/zeno/util/gfx/GFXProperties.java
@@ -1,41 +1,21 @@
 package zeno.util.gfx;
 
-import zeno.util.gfx.memory.GFXData;
-
 /**
  * The {@code GFXProperties} interface defines static properties of its respective {@link GFXSystem}.
  * 
- * @since Sep 12, 2016
  * @author Zeno
+ * @since Sep 12, 2016
+ * @version 1.0
  */
 public interface GFXProperties
 {
 	/**
-	 * Returns the maximum global fields in one {@link GFXData} object.
+	 * Returns a driver description of the {@link GFXSystem}.
 	 * 
-	 * @return  the maximum global field count
-	 */
-	public abstract int getMaximumGlobalFields();
-	
-	/**
-	 * Returns the maximum data formats in one {@link GFXData} object.
+	 * @return  a driver description
 	 * 
-	 * @return  the maximum data format count
-	 */
-	public abstract int getMaximumDataFormats();
-	
-	/**
-	 * Returns the maximum data fields in one {@link GFXData} object.
 	 * 
-	 * @return  the maximum data field count
-	 */
-	public abstract int getMaximumDataFields();
-	
-	/**
-	 * Returns the driver description of the {@link GFXSystem}.
-	 * 
-	 * @return  a system driver description
 	 * @see String
 	 */
-	public abstract String getDriver();
+	public abstract String DriverDescription();
 }
