f417eed3a2a5105be571d11b83530a3a4aa887ce
X-Zeno-X <zenothedefiant@gmail.com>
2020-05-12 04:06:49 +0200

    Initial commit.

--- src/zeno/util/gfx/GFXSystem.java
+++ src/zeno/util/gfx/GFXSystem.java
@@ -3,31 +3,39 @@ package zeno.util.gfx;
 /**
  * The {@code GFXSystem} interface defines the core functionality of a graphics system.
  * 
- * @since Sep 12, 2016
+ * 
  * @author Zeno
+ * @since Sep 12, 2016
+ * @version 1.0
  */
 public interface GFXSystem
-{
-	/**
-	 * Returns the properties of the {@code GFXSystem}.
-	 * 
-	 * @return  the system's properties
-	 * @see GFXProperties
-	 */
-	public abstract GFXProperties Properties();
-	
+{	
 	/**
 	 * Returns the memory of the {@code GFXSystem}.
 	 * 
-	 * @return  the system's memory
+	 * @return  a system memory
+	 * 
+	 * 
 	 * @see GFXMemory
 	 */
 	public abstract GFXMemory Memory();
 	
 	/**
-	 * Returns the frame of the {@code GFXSystem}.
+	 * Returns the properties of the {@code GFXSystem}.
 	 * 
-	 * @return  the system's frame
+	 * @return  system properties
+	 * 
+	 * 
+	 * @see GFXProperties
+	 */
+	public abstract GFXProperties Properties();
+	
+	/**
+	 * Returns the window of the {@code GFXSystem}.
+	 * 
+	 * @return  a rendering window
+	 * 
+	 * 
 	 * @see GFXFrame
 	 */
 	public abstract GFXFrame Window();
