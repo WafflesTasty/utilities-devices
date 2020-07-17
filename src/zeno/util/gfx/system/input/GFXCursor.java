4d2d6668c8a90c0df28f5e1e399c86085fbf1ab6
X-Zeno-X <zenothedefiant@gmail.com>
2020-07-17 17:58:56 +0200

    Initial commit.

---
+++ src/zeno/util/gfx/system/input/GFXCursor.java
@@ -0,0 +1,18 @@
+package zeno.util.gfx.system.input;
+
+import zeno.util.gfx.system.GFXMemory;
+
+/**
+ * The {@code GFXCursor} interface defines a cursor generated in {@code GFXMemory}.
+ *
+ * @author Zeno
+ * @since 17 Jul 2020
+ * @version 1.0
+ * 
+ * 
+ * @see GFXMemory
+ */
+public interface GFXCursor extends GFXMemory.Data
+{
+	// NOT APPLICABLE
+}
