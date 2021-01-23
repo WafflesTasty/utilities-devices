bc37d9097bd425c1b28020b9c3107b9f4ca8ad8a
X-Zeno-X <zenothedefiant@gmail.com>
2021-01-23 21:22:57 +0100

    Updated draw layer functionality.

--- src/zeno/util/gfx/system/window/graphics/GFXDrawLayers.java
+++ src/zeno/util/gfx/system/window/graphics/GFXDrawLayers.java
@@ -16,36 +16,9 @@ import zeno.util.tools.patterns.manipulators.Resettable;
 public interface GFXDrawLayers extends Resettable
 {
 	/**
-	 * The {@code Buffer} enum defines buffer layers.
-	 *
-	 * @author Zeno
-	 * @since 23 Jan 2021
-	 * @version 1.0
-	 */
-	public static enum Buffer
-	{
-		/**
-		 * A read buffer target.
-		 */
-		READ_BUFFER,
-		/**
-		 * A write buffer target.
-		 */
-		WRITE_BUFFER,
-		/**
-		 * Both buffer targets.
-		 */
-		BOTH_BUFFERS;
-	}
-	
-	/**
 	 * Targets a layer in the {@code GFXDrawLayers}.
 	 * 
-	 * @param buffer  a layer target
 	 * @param index   a layer index
-	 * 
-	 * 
-	 * @see Buffer
 	 */
-	public abstract void target(Buffer buffer, int index);
+	public abstract void target(int index);
 }
