f439437dd2b30c32ac18ddcba4abc912f3b1bd9a
WafflesTasty <zenothedefiant@gmail.com>
2025-03-31 09:06:52 +0200

    Initial commit, version 1.0.

---
+++ src/waffles/utils/sys/input/mouse/MouseAxis.java
@@ -0,0 +1,20 @@
+package waffles.utils.sys.input.mouse;
+
+/**
+ * The {@code MouseAxis} enum defines all axes available on mouse input.
+ *
+ * @author Waffles
+ * @since Jun 25, 2015
+ * @version 1.0
+ */
+public enum MouseAxis
+{
+	/**
+	 * The horizontal axis.
+	 */
+	X_AXIS,
+	/**
+	 * The vertical axis.
+	 */
+	Y_AXIS;
+}
